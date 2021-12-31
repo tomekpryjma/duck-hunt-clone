161df0c0b6599fcf881a74861a09f04471a8b2e4
Tomek Pryjma <tomek.pryjma@gmail.com>
2021-12-31 14:56:08 +0000

    Add level management files

---
+++ Assets/Scripts/Levels/Level.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Level
+{
+    public string name;
+    public List<Dictionary<string, float>> spawners;
+    public List<Dictionary<string, float>> specialSpawners;
+    public int spawnAmount;
+    public float spawnSpeed;
+    public Level next;
+
+    protected abstract Dictionary<string, float> Spawner(float x, float y, float direction, float spawnAmount, float spawnSpeed);
+    protected abstract Dictionary<string, float> SpecialSpawner(float x, float y, float spawnAmount);
+}
---
+++ Assets/Scripts/Levels/LevelOne.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+public class LevelOne : Level
+{
+    public LevelOne()
+    {
+        name = "A nice day";
+        spawners = new List<Dictionary<string, float>>();
+        specialSpawners = new List<Dictionary<string, float>>();
+
+        spawners.Add(Spawner(-10, 3, ((float)Direction.RIGHT), 10, 1));
+        spawners.Add(Spawner(10, 1.5f, ((float)Direction.LEFT), 5, 2));
+
+        specialSpawners.Add(SpecialSpawner(-5, -2, 2));
+    }
+
+    protected override Dictionary<string, float> Spawner(float x, float y, float direction, float spawnAmount, float spawnSpeed)
+    {
+        Dictionary<string, float> dict = new Dictionary<string, float>();
+        dict.Add("x", x);
+        dict.Add("y", y);
+        dict.Add("direction", direction);
+        dict.Add("spawnAmount", spawnAmount);
+        dict.Add("spawnSpeed", spawnSpeed);
+        return dict;
+    }
+
+    protected override Dictionary<string, float> SpecialSpawner(float x, float y, float spawnAmount)
+    {
+        Dictionary<string, float> dict = new Dictionary<string, float>();
+        dict.Add("x", x);
+        dict.Add("y", y);
+        dict.Add("spawnAmount", spawnAmount);
+        return dict;
+    }
+}
