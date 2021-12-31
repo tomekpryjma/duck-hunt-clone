fd2f20a607f6e3c0739617a467466c29659cd388
Tomek Pryjma <tomek.pryjma@gmail.com>
2021-12-31 15:05:40 +0000

    Add method for setting up scene based on current level

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,14 +5,61 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject roundOverModal;
+    [SerializeField] private GameObject spawnerPrefab;
+    [SerializeField] private GameObject specialSpawnerPrefab;
+    [SerializeField] private GameObject specialSpawnersControllerObject;
+    private SpecialMobSpawns specialSpawnersController;
     private int mobsOnThisLevel = 0;
     private int endOfRoundMobThreshold = 5;
     private bool nearEndOfRound = false;
+    private List<Level> levels;
+    private Level currentLevel;
+    private int currentLevelIndex = 0;
     public bool NearEndOfRound { get { return nearEndOfRound; } }
 
     private void Awake()
     {
         Progress.Setup();
+        levels = new List<Level>();
+
+        levels.Add(new LevelOne());
+
+        specialSpawnersController = specialSpawnersControllerObject.GetComponent<SpecialMobSpawns>();
+
+        currentLevel = levels[currentLevelIndex];
+        SceneSetup(currentLevel);
+    }
+
+    private void SceneSetup(Level level)
+    {
+        // TODO: add level name GameObject into scene
+        Debug.Log("Now playing: " + level.name);
+
+        List<GameObject> specialSpawners = new List<GameObject>();
+
+        foreach (Dictionary<string, float> s in level.spawners)
+        {
+            GameObject spawner = Instantiate(
+                spawnerPrefab,
+                new Vector3(s["x"], s["y"], 0),
+                Quaternion.identity
+            );
+
+            Spawner spawnerScript = spawner.GetComponent<Spawner>();
+            spawnerScript.Setup(s["spawnAmount"], s["spawnSpeed"], s["direction"]);
+        }
+
+        foreach (Dictionary<string, float> s in level.specialSpawners)
+        {
+            GameObject spawner = Instantiate(
+                specialSpawnerPrefab,
+                new Vector3(s["x"], s["y"], 0),
+                Quaternion.identity
+            );
+            specialSpawners.Add(spawner);
+        }
+
+        specialSpawnersController.Setup(specialSpawners, (int) level.specialSpawnAmount);
     }
 
     private void Start()
