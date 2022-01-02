a87d0e9079d325f498df63cec2f0c80ad762ae8f
Tomek Pryjma <tomek.pryjma@gmail.com>
2022-01-02 21:50:39 +0000

    Add level

---
+++ Assets/Scripts/Levels/LevelTwo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+public class LevelTwo: Level
+{
+    public LevelTwo()
+    {
+        name = "The second Level";
+        spawners = new List<Dictionary<string, float>>();
+        specialSpawners = new List<Dictionary<string, float>>();
+        specialSpawnAmount = 2;
+
+        spawners.Add(Spawner(-10, 3, ((float)Direction.RIGHT), 10, 1));
+        spawners.Add(Spawner(10, 1.5f, ((float)Direction.LEFT), 8, 2));
+        spawners.Add(Spawner(10, 0.5f, ((float)Direction.LEFT), 15, 3));
+
+        specialSpawners.Add(SpecialSpawner(-5, -2, 2));
+        specialSpawners.Add(SpecialSpawner(5, -2, 1));
+    }
+}
