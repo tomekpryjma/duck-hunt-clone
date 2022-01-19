80e29027950936a5836325cd842bb2285bd38bdc
Tomek Pryjma <tomek.pryjma@gmail.com>
2022-01-19 23:55:03 +0000

    Add helper class and methods

---
+++ Assets/Scripts/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+class Helpers : MonoBehaviour
+{
+    public static bool AnimatorIsPlaying(Animator animator)
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).length > animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+    public static void ShowScore(Vector3 pos, GameObject prefab, int score, bool isSpecial)
+    {
+        Progress.AddToStat("total_score", score);
+
+        GameObject scorePopup = Instantiate(
+            prefab,
+            pos,
+            Quaternion.identity
+        );
+        scorePopup.GetComponent<ScorePopup>().Setup(score, isSpecial);
+    }
+}
