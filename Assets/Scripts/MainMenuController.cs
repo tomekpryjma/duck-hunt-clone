2a0d831044ff2ec5ca0d97e010176967485a6cef
Tomek Pryjma <tomek.pryjma@gmail.com>
2022-01-20 00:02:26 +0000

    Refactor and clean up & add fade out

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,40 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    [SerializeField] private GameObject resumeButton;
-
-    private void Awake()
-    {
-        Debug.Log(PlayerPrefs.GetInt("currentLevel"));
-        if (PlayerPrefs.GetInt("currentLevel") == 0)
-        {
-            resumeButton.SetActive(false);
-        }
-    }
+    [SerializeField] private GameObject curtain;
 
     public void NewGame()
     {
-        Progress.NewGame();
-        SwitchScene("Main");
-    }
-    
-    public void Resume()
-    {
-        SwitchScene("Main");
+        StartCoroutine(curtain.GetComponent<Curtain>().FlushOut(GameController.NewGame, 0));
     }
 
     public void QuitGame()
     {
-        Debug.Log("Quitting");
-        Application.Quit();
-    }
-
-    private void SwitchScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
+        GameController.QuitGame();
     }
 }
